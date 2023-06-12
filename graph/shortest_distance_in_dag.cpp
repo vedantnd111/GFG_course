01732a8cabdbcc4c6c631108c1d4786033ac6862
Vedant Deshpande <vedant.deshpande@rsllearning.com>
2023-06-12 10:23:17 +0530

    find shortest distance in an direct acyclic graph using topological sort

---
+++ graph/shortest_distance_in_dag.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Find shortest distance in directed acyclic graph (DAG)
+// This solution is done using topological sort, for details regarding it refer topological_sort.cpp and topological_sort_using_dfs.cpp
+
+// we are considering distance between any 2 vertices is 1
+
+vector<int> shortest_distance_in_dag(vector<vector<int>> adj,vector<int> topologicalSort) {
+    int n = adj.size();
+    vector<int> visited(n,false);
+    vector<int> distance(n,INT_MAX);
+
+    for(int i = 0;i<topologicalSort.size();i++) {
+        for(int j = 0;j<adj[topologicalSort[i]].size();j++) {
+            if(distance[adj[topologicalSort[i]][j]] > (distance[topologicalSort[i]]+1)) {
+                distance[adj[topologicalSort[i]][j]] = distance[topologicalSort[i]]+1;
+            }
+        }
+    }
+    return distance;
+}
+
+int main()
+{
+    return 0;
+}
